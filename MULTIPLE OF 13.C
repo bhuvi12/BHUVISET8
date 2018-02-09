c51f2e01337e095cc0a34a1b39e955457e834e23
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-09 16:08:17 +0530

    multiple of 13.C

---
+++ MULTIPLE OF 13.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("\n enter the number");
+scanf("%d",&num);
+if(num%13==0)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
