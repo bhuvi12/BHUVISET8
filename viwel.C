49629b557c86a5d95cd6556e50f5e50bb31ecb16
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-09 16:30:39 +0530

    vowel.C

---
+++ viwel.C
@@ -0,0 +1,18 @@
+# include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[100];
+int i=0;
+printf("\n enter the string");
+scanf("%d",&a[i]);
+if(a[1]=='a'\\a[i]=='e'\\a[i]=='i'\\a[i]=='o'\\a[i]=='o'\\a[i]=='u'\\c[i]=='A'\\c[i]=='E'\\c[i]=='I'\\c[i]=='O'\\c[i]=='U')
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
